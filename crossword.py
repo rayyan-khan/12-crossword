e3f9f8e5aa1a9110517485a1c3b2a8f5d2ad58a3
Rayyan Khan <rayyan.khan258@gmail.com>
2019-03-20 12:00:25 -0400

    addblocks

--- crossword.py
+++ crossword.py
@@ -39,9 +39,8 @@ for arg in input:
                                match.group(3).lower()
             vWords.append((vPos, hPos, word))
 
-print('Inputs: \n Dictionary: {}\n HxW = {}x{}\n numBlocks = {}\n hWords = {}\n vWords = {}'
-      .format(wordDict, height, width, numBlocks, hWords, vWords))
-
+#print('Inputs: \n Dictionary: {}\n HxW = {}x{}\n numBlocks = {}\n hWords = {}\n vWords = {}'
+#      .format(wordDict, height, width, numBlocks, hWords, vWords))
 
 ##################
 # HELPER METHODS #
@@ -62,11 +61,13 @@ def addVword(xw, vPos, hPos, word, width):
     for k in range(len(word)):
         index = (vPos + k)*width + hPos
         if index > len(xw)-1:
-            print('Word doesn\'t fit in location.')
+            print('Word doesn\'t fit in location. (V)')
+            return -1
         wordIndexes.append(index)
     checkIndexes = {xw[i] for i in wordIndexes}
     if checkIndexes != {'-'}:
-        print('Can\'t place word over non-empty space.')
+        print('Can\'t place word over non-empty space. (V)')
+        return -1
     else:
         newXW = ''
         for k in range(len(xw)):
@@ -77,15 +78,79 @@ def addVword(xw, vPos, hPos, word, width):
         return newXW
 
 
+def addHword(xw, vPos, hPos, word, width):
+    # assumes that the word fits
+    # (for now)
+    startInd = vPos*width + hPos
+    endInd = startInd + width
+    if startInd//width != endInd//width:
+        print('Word doesn\'t fit in location (H).')
+        return -1
+    for index in range(startInd, endInd + 1):
+        if xw[index] != '-' or '~':
+            print('Can\'t add word over non-empty space (H).')
+            return -1
+    newXW = xw[:startInd] + word + xw[endInd:]
+    return newXW
+
+
+def setIndex(xw, index, character):
+    return xw[:index] + character + xw[index + 1:]
+
+
 def fillInputs(height, width, hWords, vWords):
-     abc = ['a', 'b', 'c', 'd', 'e', 'f','g', 'h', 'i', 'j',
-            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-            'u', 'v', 'w', 'x', 'y', 'z']
-     xw = ''.join([abc[num] for num in range(height*width)])
+     xw = ''.join(['-' for num in range(height*width)])
      for vWord in vWords:
         vPos, hPos, word = vWord
         xw = addVword(xw, vPos, hPos, word, width)
-     print('XW', xw)
+     for hWord in hWords:
+         vPos, hPos, word = hWord
+         xw = addHword(xw, vPos, hPos, word, width)
      printXW(xw, width)
+     return xw
 
+
+def protectBoard(xw):
+    for index in xw:
+        if xw[index] not in ('#', '-'):
+            # if it's not one of those two its a letter
+            xw = setIndex(xw, index, '~')
+    return xw
+
+
+def palindromize(xw):
+    length = len(xw)
+    for index in range(length):
+        if xw[index] == '-' and xw[length - 1 - index] == '#':
+            # if the mirror of the cell is blocked and the
+            # current one isn't, block it
+            xw = setIndex(xw, index, '#')
+        elif xw[index] == '#' and xw[length - 1 - index] == '~':
+            # if the cell is blocked and its mirror is protected
+            # then it's an impossible blocking
+            return -1 # impossible crossword
+    return xw
+
+
+def addBlocks(xw, height, width, numBlocks):
+    if height%2 + width%2 + numBlocks%2 == 3:
+        # if height, width, and numBlocks are
+        # odd, then you must place a block in the center
+        xw = setIndex(xw, int(len(xw)-1)/2, '#')
+        numBlocks = numBlocks - 1
+    else:
+        # otherwise make sure not to put block at center
+        xw = setIndex(xw, int(len(xw)-1)/2, '~')
+
+    block3 = set() # 3rd blocks away from the border
+    for index in range(len(xw)):
+        if index//width in (3, height - 3):
+            block3.add(index)
+        elif index % width in (2, width - 3):
+            block3.add(index)
+        else: continue
+    print(block3)
+
+
+# create structure
 fillInputs(height, width, hWords, vWords)
